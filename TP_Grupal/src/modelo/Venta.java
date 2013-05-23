429f1182208e379469a445ff79be08e3acf984f7
fedelopez77@gmail.com <fedelopez77@gmail.com@1e56d9d7-9fff-687d-0fd8-31264e9daa90>
2013-05-23 16:57:45 +0000

    ahora si, venta y caja

---
+++ TP_Grupal/src/modelo/Venta.java
@@ -0,0 +1,37 @@
+package modelo;
+
+import ofertas.Oferta;
+
+public class Venta {
+
+	public void agregarProducto(Producto prod) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setMedioDePago(String medio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void finalizarVenta() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public float getTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getTotalDescuentos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void aplicarOferta(Oferta oferta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
