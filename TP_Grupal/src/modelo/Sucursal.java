b119f68ca13a3003366137af5cdbd2c5afeb2a0b
fedelopez77@gmail.com <fedelopez77@gmail.com@1e56d9d7-9fff-687d-0fd8-31264e9daa90>
2013-05-30 19:37:09 +0000

    correccioens

--- TP_Grupal/src/modelo/Sucursal.java
+++ TP_Grupal/src/modelo/Sucursal.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 import ofertas.OfertaPorUnidadFactory;
 //import ofertas.OfertaPorVentaTotalFactory;
-//import ofertas.OfertaPorVolumenFactory;
+import ofertas.OfertaPorVolumenFactory;
 
 public class Sucursal {
 	
@@ -26,14 +26,14 @@ public class Sucursal {
 	
 	public void cargarOfertas() {
 		OfertaPorUnidadFactory unidadFactory = new OfertaPorUnidadFactory();
-		//OfertaPorVolumenFactory volumenFactory = new OfertaPorVolumenFactory();
+		OfertaPorVolumenFactory volumenFactory = new OfertaPorVolumenFactory();
 		// TODO DESCOMENTAR!!!!!!!!!!
 		//OfertaPorVentaTotalFactory ventaTotalFactory = new OfertaPorVentaTotalFactory();
 		unidadFactory.cargarOfertas();
-		//volumenFactory.cargarOfertas();
+		volumenFactory.cargarOfertas();
 		//ventaTotalFactory.cargarOfertas();
 		caja.cargarOfertasUnidad( unidadFactory.getOfertas() );
-		//caja.cargarOfertasVolumen( volumenFactory.getOfertas() );
+		caja.cargarOfertasVolumen( volumenFactory.getOfertas() );
 		//caja.cargarOfertasVentaTotal( ventaTotalFactory.getOfertas() );
 	}
 	
