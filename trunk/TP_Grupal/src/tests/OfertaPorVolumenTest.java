091aa426575857a54af854a0481446b9a6a9f2f2
lucasawad@gmail.com <lucasawad@gmail.com@1e56d9d7-9fff-687d-0fd8-31264e9daa90>
2013-05-30 03:47:28 +0000

    Más test para la oferta por volumen.

--- trunk/TP_Grupal/src/tests/OfertaPorVolumenTest.java
+++ trunk/TP_Grupal/src/tests/OfertaPorVolumenTest.java
@@ -8,7 +8,6 @@ import junit.framework.TestCase;
 import modelo.Descuento;
 import modelo.Producto;
 import modelo.ProductoVendido;
-import modelo.Venta;
 
 import ofertas.OfertaPorVolumen;
 import org.junit.Test;
@@ -47,12 +46,25 @@ public class OfertaPorVolumenTest extends TestCase{
 		ProductoVendido productosVendidos = new ProductoVendido(producto, 4);
 		OfertaPorVolumen oferta = new OfertaPorVolumen();
 		oferta.addProducto(producto, 2, 1);
-		ArrayList<ProductoVendido> listaDeProductos = new ArrayList<ProductoVendido>();
+		List<ProductoVendido> listaDeProductos = new ArrayList<ProductoVendido>();
 		listaDeProductos.add(productosVendidos);
 		
 		List<Descuento> descuentos = oferta.aplicarOferta(listaDeProductos);
 		
 		assertEquals(20, descuentos.get(0).getDescuento(), 1e-3);
+	}
+	
+	@Test
+	public void testOfertaUnProductoTeLlevasOtroDistintoGratisTieneElValorDeDescuentoDelProducto() throws ExcepcionCantidadInvalida {
+		Producto producto1 = new Producto("nombre1", "marca1", "categoria1", 10);
+		Producto producto2 = new Producto("nombre2", "marca2", "categoria2", 20);
+		OfertaPorVolumen oferta = new OfertaPorVolumen();
+		oferta.addProducto(producto1, 1, 0);
+		oferta.addProducto(producto2, 1, 1);
+		
+		float descuento = oferta.getValorDescuento();
+		
+		assertEquals(20, descuento, 1e-3);
 	}
 
 }
