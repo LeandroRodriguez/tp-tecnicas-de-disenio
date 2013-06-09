d0c3b3cdf80c106138a269e29a728967c6102ad8
lucasawad@gmail.com <lucasawad@gmail.com@1e56d9d7-9fff-687d-0fd8-31264e9daa90>
2013-06-09 23:27:24 +0000

    Pruebas del Escenario 1

--- trunk/nuevo tp del otro/src/tp_supermarket/tests/TestPuntos.java
+++ trunk/nuevo tp del otro/src/tp_supermarket/tests/TestPuntos.java
@@ -11,8 +11,6 @@ import tp_supermarket.producto.Producto;
 import tp_supermarket.restriccion.Restriccion;
 import tp_supermarket.restriccion.RestriccionNombreProducto;
 
-import static org.junit.Assert.*;
-
 
 public class TestPuntos {
 	
@@ -29,12 +27,48 @@ public class TestPuntos {
 		misproducts.add(coca);
 		ArrayList<Producto> descuentos = bonif.bonificar(misproducts, res);
 		int total = 0;
-		System.out.format("%d\n", descuentos.size());
 		for (Producto descuento : descuentos) {
 			total += descuento.getCosto();
 		}
 		
 		Assert.assertEquals(15, total);
+	}
+	
+	@Test
+	public void testEscenario1TvFlatEntrega100Puntos() {
+		ArrayList<Restriccion> restricciones = new ArrayList<Restriccion>();
+		restricciones.add(new RestriccionNombreProducto("TV FLAT"));
+		BonificacionPuntos bonif = new BonificacionPuntos(100, restricciones);
+		Producto tv = new Producto(1, "TV FLAT", 10.0f);
+		
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		productos.add(tv);
+		ArrayList<Producto> descuentos = bonif.bonificar(productos, restricciones);
+		int total = 0;
+		for (Producto descuento : descuentos) {
+			total += descuento.getCosto();
+		}
+		
+		Assert.assertEquals(100, total);
+	}
+	
+	@Test
+	public void testEscenario1DiezCocasEntregan1PuntoAlComprarDoce() {
+		ArrayList<Restriccion> restricciones = new ArrayList<Restriccion>();
+		restricciones.add(new RestriccionNombreProducto("Coca Cola", 10));
+		BonificacionPuntos bonif = new BonificacionPuntos(1, restricciones);
+		Producto coca = new Producto(1, "Coca Cola", 10.0f);
+		
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		for(int i = 0; i < 12; i++)
+			productos.add(coca);
+		ArrayList<Producto> descuentos = bonif.bonificar(productos, restricciones);
+		int total = 0;
+		for (Producto descuento : descuentos) {
+			total += descuento.getCosto();
+		}
+		
+		Assert.assertEquals(1, total);
 	}
 	
 	
